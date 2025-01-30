25cd1048c51da4ce0ad632fe1a81b11202a840a0
Rabiya-Kamran <rabiya.kamran@folium.ai>
2025-01-30 23:37:15 +0500

    Configured for deployment for amazon linux

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,40 +1,66 @@
 #!/usr/bin/env bash
 
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status
 
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/Rabiya-Kamran/ProfilesApi'
-
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-# Set Ubuntu Language
-locale-gen en_GB.UTF-8
+# Set system locale (Amazon Linux does not have locale-gen)
+echo "Setting system locale..."
+sudo localectl set-locale LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 
-# Install Python, SQLite and pip
+# Install required dependencies
 echo "Installing dependencies..."
-apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+sudo dnf update -y
+sudo dnf install -y python3 python3-devel python3-pip python3-virtualenv sqlite nginx git
 
-mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+# Create project directory and clone repo
+echo "Setting up project..."
+sudo mkdir -p $PROJECT_BASE_PATH
+sudo git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
+# Set up Python virtual environment
+echo "Creating virtual environment..."
 python3 -m venv $PROJECT_BASE_PATH/env
+source $PROJECT_BASE_PATH/env/bin/activate
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Install project dependencies
+echo "Installing Python dependencies..."
+pip install --upgrade pip
+pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
 
-# Run migrations
+# Run Django migrations
+echo "Running migrations..."
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
-# Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl restart profiles_api
+# Create systemd service for uWSGI
+echo "Setting up uWSGI systemd service..."
+cat <<EOT | sudo tee /etc/systemd/system/profiles_api.service
+[Unit]
+Description=Profiles API uWSGI Service
+After=network.target
 
-# Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-systemctl restart nginx.service
+[Service]
+User=ec2-user
+Group=ec2-user
+WorkingDirectory=$PROJECT_BASE_PATH
+Environment="PATH=$PROJECT_BASE_PATH/env/bin"
+ExecStart=$PROJECT_BASE_PATH/env/bin/uwsgi --ini $PROJECT_BASE_PATH/deploy/uwsgi.ini
 
-echo "DONE! :)"
+[Install]
+WantedBy=multi-user.target
+EOT
+
+# Reload systemd to recognize the new service
+sudo systemctl daemon-reload
+sudo systemctl enable profiles_api
+sudo systemctl start profiles_api
+
+# Setup nginx to serve the application
+echo "Configuring Nginx..."
+sudo cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/conf.d/profiles_api.conf
+sudo systemctl restart nginx
+
+echo "Deployment complete! 🎉"
