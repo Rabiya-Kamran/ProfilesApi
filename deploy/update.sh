9ecc03968483fae71408a0c69b480ab8395262b4
Rabiya-Kamran <rabiya.kamran@folium.ai>
2025-01-31 12:42:23 +0500

    Updated update.sh for amazon linux

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,10 +4,18 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
+# Navigate to the project directory
 cd $PROJECT_BASE_PATH
+
+# Pull latest changes from git
 git pull
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+
+# Activate virtual environment and run migrations
+source $PROJECT_BASE_PATH/env/bin/activate
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+# Restart Supervisor-managed process
+sudo systemctl restart supervisord
 
 echo "DONE! :)"
